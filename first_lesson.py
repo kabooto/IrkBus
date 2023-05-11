57487566e8b93c5029113216dd05973d95b5dddf
Alex Sher <sher6259@mail.ru>
2023-05-11 13:54:45 +0800

    Little addungs

--- first_lesson.py
+++ first_lesson.py
@@ -5,22 +5,24 @@ import webbrowser
 bot = telebot.TeleBot('6175521547:AAHPf26Kd4zWMf5w59QZ7iqnQ8Nyk_g1w3M')
 
 
-#  open weblink
+#  Opening weblink
 @bot.message_handler(commands=['website'])
 def website(message):
     webbrowser.open('https://google.com')
 
 
-# replying to message
+# Replying to message
 @bot.message_handler()
 def info(message):
     if message.text.lower() == 'id':
         bot.reply_to(message, f'Your ID:{message.from_user.id}')
     elif message.text.lower() == '/start':
         main(message)
+    elif message.text.lower() == '/text':
+        format_text(message)
 
 
-# replying to commands with info about user
+# Replying to commands with info about user
 @bot.message_handler(commands=['start'])
 def main(message):
     bot.send_message(message.chat.id, f'Hello! {message.from_user.first_name} {message.from_user.last_name}')
